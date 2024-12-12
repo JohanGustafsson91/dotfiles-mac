524fd93144c830700102939cc93fc0758fc2741c
Johan Gustafsson <johan.gustafsson91@gmail.com>
2024-12-12 20:44:16 +0100

    update scripts

--- scripts/fuzzy-select-project.sh
+++ scripts/fuzzy-select-project.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-selected_dir=$(find ~/code -path "*/node_modules" -prune -o -maxdepth 3 \( -type f -name "package.json" -o -type d -name ".git" \) -exec dirname {} \; | sort -u | fzf --prompt="Select project: ")
+selected_dir=$(find ~/code -path "*/node_modules" -prune -o -maxdepth 3 \( -type f -name "package.json" -o -type d -name ".git" \) -exec dirname {} \; | sort -u | awk -F'/' '{print $4"/"$5"/"$6}' | fzf --prompt="Select project: ")
 
 if [[ -z "$selected_dir" ]]; then
   echo "No folder selected."
   exit 1
 fi
 
-cd "$selected_dir" || exit
+cd ~/"$selected_dir" || exit
 
 echo "Run script from: $selected_dir"
 bash ~/.config/scripts/code-in-tmux.sh
